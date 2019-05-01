d115e6aeaf0bbfb04c1e466d45ae4480a8a952d2
Jefferson Silva <jgsnat@gmail.com>
2019-05-01 11:14:32 -0300

    initial commit;

---
+++ front-end/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render() {
+
+        return (
+            <div>
+                React!
+            </div>
+        );
+
+    }
+
+}
+
+export default App;
---
+++ front-end/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+// imgs
+import './img/favicon.ico';
+
+// css
+import './css/index.css';
+import './css/pure-min.css';
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#main")
+);
