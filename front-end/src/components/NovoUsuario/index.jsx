b7f52f93b64287210bc5830e1ae33e2176a29405
Jefferson Silva <jgsnat@gmail.com>
2019-05-01 14:27:10 -0300

    - component Label and refactoring of NovoUsuario;

--- front-end/src/components/NovoUsuario/index.jsx
+++ front-end/src/components/NovoUsuario/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Label from '../Label'
 
 class NovoUsuario extends React.Component {
 
     render() {
         return (
             <div className="center">
+                <form>
+                    <Label htmlFor="nome" texto="Quem é você?" />
+                </form>
             </div>
         )
     }
