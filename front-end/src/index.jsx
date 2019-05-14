26d58ed4d92bcf7989da58f1ff792143a60063a0
Jefferson Silva <jgsnat@gmail.com>
2019-05-13 21:19:32 -0300

    - application install code;

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -5,6 +5,15 @@ import App from './components/App';
 
 // imgs
 import './img/favicon.ico';
+import './img/icon-72x72.png';
+import './img/icon-96x96.png';
+import './img/icon-128x128.png';
+import './img/icon-144x144.png';
+import './img/icon-152x152.png';
+import './img/icon-192x192.png';
+import './img/icon-384x384.png';
+import './img/icon-512x512.png';
+
 
 // css
 import './css/index.css';
